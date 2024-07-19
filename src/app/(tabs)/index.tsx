542669807f52e455c50e7bc0696997545abcfeb4
sergiorbotelho <sergiorbotelho@gmail.com>
2024-07-18 21:30:44 -0300

    feat: Add input component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { Input } from "@/components/input";
 import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 bg-gray-900">
-      <Text>HOME</Text>
+    <View className="flex-1 bg-gray-900 pt-14 p4">
+      <Input>
+        <Input.Field placeholder="Pesquisar no e-mail" />
+      </Input>
     </View>
   );
 }
