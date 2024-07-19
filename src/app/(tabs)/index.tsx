5c2b1c462b2dc58370586b57d60074b6cf567010
sergiorbotelho <sergiorbotelho@gmail.com>
2024-07-18 21:54:00 -0300

    feat: Add Avatar component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
+import { Avatar } from "@/components/avatar";
 import { Input } from "@/components/input";
 import { MenuButton } from "@/components/menu-button";
 import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 bg-gray-900 pt-14 p4">
+    <View className="flex-1 bg-gray-900 pt-14 p-4">
       <Input>
         <MenuButton />
         <Input.Field placeholder="Pesquisar no e-mail" />
+        <Avatar
+          source={{ uri: "https://github.com/sergiorbotelho.png" }}
+          size="small"
+        />
       </Input>
     </View>
   );
