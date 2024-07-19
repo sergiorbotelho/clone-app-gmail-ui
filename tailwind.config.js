ebaf1413b3a80814f991c356c859ddda4765cabb
sergiorbotelho <sergiorbotelho@gmail.com>
2024-07-18 21:20:20 -0300

    chore: font customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import { colors } from "./src/styles/colors";
+import { fontFamily } from "./src/styles/font-family";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
@@ -6,6 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors,
+      fontFamily,
     },
   },
   plugins: [],
